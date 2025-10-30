b488bfcab3d549d5c7390c472f910e564d18ddd0
dushdhanu <dushmanthidananjana4@gmail.com>
2025-10-30 13:12:02 +0530

    I changed some codes in RegisterDTO class

--- Local_Pharmacy_Stock_and_Delivery_System/src/main/java/ac/nsbm/local_pharmacy_stock_and_delivery_system/dto/RegisterDTO.java
+++ Local_Pharmacy_Stock_and_Delivery_System/src/main/java/ac/nsbm/local_pharmacy_stock_and_delivery_system/dto/RegisterDTO.java
@@ -11,41 +11,41 @@ public class RegisterDTO {
     private ac.nsbm.local_pharmacy_stock_and_delivery_system.entity.Role role;
 
     @lombok.Generated
-    public RegisterDTO() { /* compiled code */ }
+    public RegisterDTO() {}
 
     @lombok.Generated
-    public java.lang.String getName() { /* compiled code */ }
+    public java.lang.String getName() {}
 
     @lombok.Generated
-    public java.lang.String getEmail() { /* compiled code */ }
+    public java.lang.String getEmail() {}
 
     @lombok.Generated
-    public java.lang.String getPassword() { /* compiled code */ }
+    public java.lang.String getPassword() {}
 
     @lombok.Generated
-    public ac.nsbm.local_pharmacy_stock_and_delivery_system.entity.Role getRole() { /* compiled code */ }
+    public ac.nsbm.local_pharmacy_stock_and_delivery_system.entity.Role getRole() {}
 
     @lombok.Generated
-    public void setName(java.lang.String name) { /* compiled code */ }
+    public void setName(java.lang.String name) {}
 
     @lombok.Generated
-    public void setEmail(java.lang.String email) { /* compiled code */ }
+    public void setEmail(java.lang.String email) {}
 
     @lombok.Generated
-    public void setPassword(java.lang.String password) { /* compiled code */ }
+    public void setPassword(java.lang.String password) {}
 
     @lombok.Generated
-    public void setRole(ac.nsbm.local_pharmacy_stock_and_delivery_system.entity.Role role) { /* compiled code */ }
+    public void setRole(ac.nsbm.local_pharmacy_stock_and_delivery_system.entity.Role role) {}
 
     @lombok.Generated
-    public boolean equals(java.lang.Object o) { /* compiled code */ }
+    public boolean equals(java.lang.Object o) {}
 
     @lombok.Generated
-    protected boolean canEqual(java.lang.Object other) { /* compiled code */ }
+    protected boolean canEqual(java.lang.Object other) {}
 
     @lombok.Generated
-    public int hashCode() { /* compiled code */ }
+    public int hashCode() {}
 
     @lombok.Generated
-    public java.lang.String toString() { /* compiled code */ }
+    public java.lang.String toString() {}
 }
