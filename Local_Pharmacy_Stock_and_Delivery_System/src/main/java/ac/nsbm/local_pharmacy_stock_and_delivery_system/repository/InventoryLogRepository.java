f7e3676b724130293cff64fb31488c650dc75583
dushdhanu <dushmanthidananjana4@gmail.com>
2025-10-31 08:45:31 +0530

    I create a InventoryLogRepository Interface in repository package

---
+++ Local_Pharmacy_Stock_and_Delivery_System/src/main/java/ac/nsbm/local_pharmacy_stock_and_delivery_system/repository/InventoryLogRepository.java
@@ -0,0 +1,4 @@
+package ac.nsbm.local_pharmacy_stock_and_delivery_system.repository;
+
+public interface InventoryLogRepository extends org.springframework.data.jpa.repository.JpaRepository<ac.nsbm.local_pharmacy_stock_and_delivery_system.entity.InventoryLog,java.lang.Long> {
+}
