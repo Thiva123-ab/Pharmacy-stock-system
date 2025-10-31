3d1e4d68566ecaced3c2f2a87eed8a11fd80382e
dushdhanu <dushmanthidananjana4@gmail.com>
2025-10-31 11:12:34 +0530

    I changed some codes

--- Local_Pharmacy_Stock_and_Delivery_System/src/main/java/ac/nsbm/local_pharmacy_stock_and_delivery_system/repository/MedicineRepository.java
+++ Local_Pharmacy_Stock_and_Delivery_System/src/main/java/ac/nsbm/local_pharmacy_stock_and_delivery_system/repository/MedicineRepository.java
@@ -1,5 +1,10 @@
 package ac.nsbm.local_pharmacy_stock_and_delivery_system.repository;
 
-public interface MedicineRepository extends org.springframework.data.jpa.repository.JpaRepository<ac.nsbm.local_pharmacy_stock_and_delivery_system.entity.Medicine,java.lang.Long> {
-    org.springframework.data.domain.Page<ac.nsbm.local_pharmacy_stock_and_delivery_system.entity.Medicine> findByNameContainingIgnoreCase(java.lang.String q, org.springframework.data.domain.Pageable pageable);
+import ac.nsbm.local_pharmacy_stock_and_delivery_system.entity.Medicine;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface MedicineRepository extends JpaRepository<Medicine, Long> {
+    Page<Medicine> findByNameContainingIgnoreCase(String q, Pageable pageable);
 }
