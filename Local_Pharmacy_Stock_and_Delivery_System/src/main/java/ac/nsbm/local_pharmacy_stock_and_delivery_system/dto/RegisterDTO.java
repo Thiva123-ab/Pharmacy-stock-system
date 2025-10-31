109e9cbb160ad0c126b9ced3de4446082449611d
dushdhanu <dushmanthidananjana4@gmail.com>
2025-10-31 10:34:27 +0530

    I changed some codes

--- Local_Pharmacy_Stock_and_Delivery_System/src/main/java/ac/nsbm/local_pharmacy_stock_and_delivery_system/dto/RegisterDTO.java
+++ Local_Pharmacy_Stock_and_Delivery_System/src/main/java/ac/nsbm/local_pharmacy_stock_and_delivery_system/dto/RegisterDTO.java
@@ -1,51 +1,20 @@
 package ac.nsbm.local_pharmacy_stock_and_delivery_system.dto;
 
+import ac.nsbm.local_pharmacy_stock_and_delivery_system.entity.Role;
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
+import lombok.Data;
+
+@Data
 public class RegisterDTO {
-    @jakarta.validation.constraints.NotBlank
-    private java.lang.@jakarta.validation.constraints.NotBlank String name;
-    @jakarta.validation.constraints.Email
-    @jakarta.validation.constraints.NotBlank
-    private java.lang.@jakarta.validation.constraints.Email @jakarta.validation.constraints.NotBlank String email;
-    @jakarta.validation.constraints.NotBlank
-    private java.lang.@jakarta.validation.constraints.NotBlank String password;
-    private ac.nsbm.local_pharmacy_stock_and_delivery_system.entity.Role role;
+    @NotBlank
+    private String name;
 
-    @lombok.Generated
-    public RegisterDTO() {}
+    @Email @NotBlank
+    private String email;
 
-    @lombok.Generated
-    public java.lang.String getName() {}
+    @NotBlank
+    private String password;
 
-    @lombok.Generated
-    public java.lang.String getEmail() {}
-
-    @lombok.Generated
-    public java.lang.String getPassword() {}
-
-    @lombok.Generated
-    public ac.nsbm.local_pharmacy_stock_and_delivery_system.entity.Role getRole() {}
-
-    @lombok.Generated
-    public void setName(java.lang.String name) {}
-
-    @lombok.Generated
-    public void setEmail(java.lang.String email) {}
-
-    @lombok.Generated
-    public void setPassword(java.lang.String password) {}
-
-    @lombok.Generated
-    public void setRole(ac.nsbm.local_pharmacy_stock_and_delivery_system.entity.Role role) {}
-
-    @lombok.Generated
-    public boolean equals(java.lang.Object o) {}
-
-    @lombok.Generated
-    protected boolean canEqual(java.lang.Object other) {}
-
-    @lombok.Generated
-    public int hashCode() {}
-
-    @lombok.Generated
-    public java.lang.String toString() {}
+    private Role role = Role.ROLE_CUSTOMER;
 }
