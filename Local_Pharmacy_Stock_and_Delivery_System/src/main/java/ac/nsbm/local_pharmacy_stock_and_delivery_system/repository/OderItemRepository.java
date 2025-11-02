60bb994d0619ffe6fdcfec681ab27c4a1fba94a3
dushdhanu <dushmanthidananjana4@gmail.com>
2025-11-02 14:26:10 +0530

    Change some codes

--- Local_Pharmacy_Stock_and_Delivery_System/src/main/java/ac/nsbm/local_pharmacy_stock_and_delivery_system/repository/OderItemRepository.java
+++
@@ -1,7 +0,0 @@
-package ac.nsbm.local_pharmacy_stock_and_delivery_system.repository;
-
-import ac.nsbm.local_pharmacy_stock_and_delivery_system.entity.OrderItem;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface OrderItemRepository extends JpaRepository<OrderItem, Long> {
-}
