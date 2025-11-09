c0c403a68c269e959b22eaab6408114ffbf5ace4
dushdhanu <dushmanthidananjana4@gmail.com>
2025-11-09 22:16:42 +0530

    Create ChatMessageResponseDTO class in dto package

---
+++ Local_Pharmacy_Stock_and_Delivery_System/src/main/java/ac/nsbm/local_pharmacy_stock_and_delivery_system/dto/ChatMessageResponseDTO.java
@@ -0,0 +1,32 @@
+package ac.nsbm.local_pharmacy_stock_and_delivery_system.dto;
+
+import ac.nsbm.local_pharmacy_stock_and_delivery_system.entity.ChatMessage;
+import ac.nsbm.local_pharmacy_stock_and_delivery_system.entity.Role;
+import lombok.Data;
+import java.time.LocalDateTime;
+
+@Data
+public class ChatMessageResponseDTO {
+    private Long id;
+    private String content;
+    private LocalDateTime timestamp;
+    private Long senderId;
+    private String senderName;
+    private Role senderRole;
+    private Long orderId;
+    private String chatType; // --- ADDED THIS FIELD ---
+
+    // Factory method to convert an Entity to a DTO
+    public static ChatMessageResponseDTO fromEntity(ChatMessage message) {
+        ChatMessageResponseDTO dto = new ChatMessageResponseDTO();
+        dto.setId(message.getId());
+        dto.setContent(message.getContent());
+        dto.setTimestamp(message.getTimestamp());
+        dto.setOrderId(message.getOrder().getId());
+        dto.setSenderId(message.getSender().getId());
+        dto.setSenderName(message.getSender().getFirstName() + " " + message.getSender().getLastName());
+        dto.setSenderRole(message.getSender().getRole());
+        dto.setChatType(message.getChatType()); // --- ADDED THIS LINE ---
+        return dto;
+    }
+}
